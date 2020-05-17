b037a65adefd654e85908da2a309a9a4c6de7ebf
Landolphia <landolphia@users.noreply.github.com>
2020-05-17 16:28:51 -0400

    Basic login

--- HTMLScrape.py
+++ HTMLScrape.py
@@ -5,12 +5,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from selenium.webdriver.common.keys import Keys
-#from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
-#from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-#from selenium.common.exceptions import NoSuchElementException
 
 DEBUG = 1
 
@@ -27,8 +24,8 @@ class Payload:
                 print ("Fixed:", data[0].rstrip(), " / ", data[1].rstrip())
             self.user_id = "username"
             self.pass_id = "password"
-            #self.button = "//input[@type='submit']"
-            #self.button = "//div/div/a[@href='#']"
+            self.login_link = "//form[@id=\"login\"]/preceding-sibling::a"
+            self.button = "//input[@type=\"submit\"]"
             self.form = "login"
             self.username = data[0].rstrip()
             self.password = data[1].rstrip()
@@ -52,25 +49,23 @@ def login(driver):
     print("Waiting for page to load.")
     wait.until(EC.presence_of_element_located((By.ID, payload.form)))
     print("Page loaded.")
-    link = driver.find_element(By.XPATH, "//form[@id=\"login\"]/preceding-sibling::a")
-    print("Link: ", link)
+    link = driver.find_element(By.XPATH, payload.login_link)
+    print("Clicking on link to show form.")
     link.click()
 
-    print("Waiting for form to load. [", payload.user_id, "]")
+    print("Waiting for form to load.")
     wait.until(EC.presence_of_element_located((By.ID, payload.user_id)))
-    username = driver.find_element_by_id(payload.user_id)
-    print ("Username = ", username)
-
-    password = driver.find_element_by_id(payload.pass_id)
-    print ("Password = ", password)
+    print("Loaded.")
 
     driver.find_element_by_id(payload.user_id).send_keys(payload.username)
 
     driver.find_element_by_id(payload.pass_id).send_keys(payload.password)
 
-    #result = driver.find_element(By.XPATH, payload.button).click()
+    print("Waiting for submit button to load.")
+    wait.until(EC.presence_of_element_located((By.XPATH, payload.button)))
+    print("Loaded.")
 
-    input("Press enter.")
+    result = driver.find_element(By.XPATH, payload.button).click()
 
     return result
 
@@ -85,11 +80,8 @@ def main():
     else:
         options.headless = True
 
-    #driver = webdriver.Remote(
-    #    command_executor='http://127.0.0.1:9515',
-    #    desired_capabilities=DesiredCapabilities.CHROME)
 
-    driver = webdriver.Chrome()#'./chromedriver.exe', options=options)
+    driver = webdriver.Chrome()
     if DEBUG != None:
         print ("Driver: ", driver)
     print ("Result: \n", login(driver))
@@ -98,5 +90,6 @@ def main():
     driver.quit()
     
     print ("\nDone in %s seconds." % (time.time() - start_time))
+    input("Press enter.")
 
 main()
